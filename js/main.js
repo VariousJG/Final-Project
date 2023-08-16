7b692764ba7a086b63800b200cdd32982e8693da
jon gay <Jonathan.s.gay@hotmail.co.uk>
2023-08-16 12:38:12 +0100

    updated CSS and HTML, minor JS updates

--- js/main.js
+++ js/main.js
@@ -9,9 +9,46 @@ const categoryFilters = document.querySelectorAll('.nav-icon')
 
 const textInput = document.querySelector('#compendium-search');
 const searchForm = document.querySelector('.searchFunction');
+const container = document.querySelector('#entries-container');
+
+// Opening Screen with all items
+axios.get(`https://botw-compendium.herokuapp.com/api/v3/compendium/all`)
+    .then(response => {
+
+        const compendiumArray = response.data.data;
+
+        compendiumArray.forEach(item => {
+            // console.log(item);
+
+            const divTag = document.createElement('div');
+            divTag.innerHTML = `
+
+            <div class="compendium-result">
+                <img class="compendium-img" src="${item.image}" data-id="${item.id}">
+                <p class="hylia-font">${item.name}</p>
+                <p class="hylia-font">${item.id}</p>
+            </div>
+                `
+
+
+            container.appendChild(divTag);
+        });
+
+    });
+
+// Modal
+container.addEventListener('click', function (event) {
+    console.log(event.target.dataset.id);
+    axios.get(`https://botw-compendium.herokuapp.com/api/v3/compendium/entry/${item.id}`)
+
+        .then(response => {
 
 
 
+        })
+});
+
+// Filter using the Nav Icons
 categoryFilters.forEach(item => {
     item.addEventListener('click', function (event) {
         console.log(item.id);
@@ -20,18 +57,18 @@ categoryFilters.forEach(item => {
             .then(response => {
 
                 const compendiumArray = response.data.data;
-                const container = document.querySelector('#filler');
                 container.innerHTML = "";
 
                 compendiumArray.forEach(item => {
-                    console.log(item);
 
                     const divTag = document.createElement('div');
                     divTag.innerHTML = `
-                        <img class="compendium-img" src="${item.image}">
+                    <div class="compendium-result">
+                        <img class="compendium-img" src="${item.image}" data-id="${item.id}">
                         <p class="hylia-font">${item.name}</p>
                         <p class="hylia-font">${item.id}</p>
-                `
+                    </div>
+                    `;
                     container.appendChild(divTag);
                 });
 
@@ -40,30 +77,7 @@ categoryFilters.forEach(item => {
     });
 });
 
-axios.get(`https://botw-compendium.herokuapp.com/api/v3/compendium/all`)
-    .then(response => {
-
-        const compendiumArray = response.data.data;
-
-        compendiumArray.forEach(item => {
-            console.log(item);
-
-            const divTag = document.createElement('div');
-            divTag.innerHTML = `
-
-                        <img class="compendium-img" src="${item.image}">
-                        <p class="hylia-font">${item.name}</p>
-                        <p class="hylia-font">${item.id}</p>
-                `
-
-            const container = document.querySelector('#filler');
-            container.appendChild(divTag);
-        });
-
-    });
-
-
-
+// Search
 searchForm.addEventListener('submit', function (event) {
     event.preventDefault()
     const userSearch = textInput.value;
@@ -75,27 +89,29 @@ searchForm.addEventListener('submit', function (event) {
             console.log(response);
 
             const item = response.data.data;
-            const container = document.querySelector('#filler');
+            const container = document.querySelector('#entries-container');
             container.innerHTML = "";
 
             const divTag = document.createElement('div');
             divTag.innerHTML = `
-                <img class="compendium-img" src="${item.image}">
+
+            <div class="compendium-result">
+                <img class="compendium-img" src="${item.image}" data-id="${item.id}">
                 <p class="hylia-font">${item.name}</p>
                 <p class="hylia-font">${item.id}</p>
-        `
+            </div>
+            `
             container.appendChild(divTag);
 
         });
 });
 
+
+
 /*
 Psuedo
-- I need a function that works on the Icons and loads a full list off all item in that class
 
 - A modal that shows the relevant stats and Image of the entry selected. Will need a button to close it once selected
-
-- A search Function
 
 - The Favourite button is a requirment but I don't know how that works
 
